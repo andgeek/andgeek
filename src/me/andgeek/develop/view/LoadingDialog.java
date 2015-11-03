a4238c2b48cb6b42d44f0f3f41fbe6dc4d393d34
andgeek <358072651@qq.com>
2015-11-03 14:59:42 +0800

    修改网络请求对话框

---
+++ src/me/andgeek/develop/view/LoadingDialog.java
@@ -0,0 +1,63 @@
+package me.andgeek.develop.view;
+
+import me.andgeek.develop.R;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LoadingDialog extends Dialog {
+    
+    private ImageView mImageView;
+    
+    private Animation mAnimation;
+    
+    private TextView mTvMsg;
+    
+    public LoadingDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+    
+    public LoadingDialog(Context context, int theme) {
+        super(context, R.style.Translucent_NoTitle);
+    }
+    
+    public LoadingDialog(Context context) {
+        this(context, -1);
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_loading);
+        setCanceledOnTouchOutside(false);
+        
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.loading_rotate);
+        LinearInterpolator lin = new LinearInterpolator();
+        mAnimation.setInterpolator(lin);
+        mTvMsg = (TextView) findViewById(R.id.tv_msg);
+    }
+    
+    public void show(String msg) {
+        super.show();
+        mTvMsg.setText(msg);
+        mImageView.post(new Runnable() {
+            @Override
+            public void run() {
+                mImageView.startAnimation(mAnimation);
+            }
+        });
+    }
+    
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        mImageView.clearAnimation();
+    }
+}
